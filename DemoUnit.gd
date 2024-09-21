3302daee47a581396be6375801bf820a97b02b10
Malf <malte@alfi.li>
2024-09-21 20:40:19 +0200

    fix demoScene

--- DemoUnit.gd
+++ DemoUnit.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 var attack_range = 8
 var projectile_speed = 10
 var attack_damage = 2
-var speed = 10
+var movement_speed = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
